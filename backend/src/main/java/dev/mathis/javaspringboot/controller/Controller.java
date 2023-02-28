29faa000486b541f1ce8378b1a2c08d316cde8d2
Zenika <zenika@MacBook-Pro-de-Zenika.local>
2023-02-28 14:21:51 +0100

    Upload simple VueJS+Spring boot project

---
+++ backend/src/main/java/dev/mathis/javaspringboot/controller/Controller.java
@@ -0,0 +1,15 @@
+package dev.mathis.javaspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "spring boot vuejs app";
+    }
+}
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+        msg: ''
+    }
+  },
+  mounted() {
+    fetch('/api/hello')
+    .then(res => res.text())
+    .then(data => {
+        this.msg = data;
+    })
+  }
+}
+</script>
