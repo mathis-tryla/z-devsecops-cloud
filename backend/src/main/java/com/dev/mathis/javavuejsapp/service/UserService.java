18fe67d9344f8315e428213d18e7a912f2122196
Mathis_Tryla <mathistryla@gmail.com>
2023-03-10 11:44:29 +0100

    SQL and API communications working

--- backend/src/main/java/com/dev/mathis/javavuejsapp/service/UserService.java
+++ backend/src/main/java/com/dev/mathis/javavuejsapp/service/UserService.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    //@Autowired
+    @Autowired
     private UserRepository userRepository;
 
     public List<User> getUsers() {
