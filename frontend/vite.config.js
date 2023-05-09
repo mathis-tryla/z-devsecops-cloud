fdf97f29709cc3109fa0d5749bf05d4325243e3a
Mathis_Tryla <mathistryla@gmail.com>
2023-05-09 15:24:26 +0200

    fix(bad-gateway-vuejs-accept-header): add nginx rendering server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
+  }/*,
   server: {
     port: 3000,
     proxy: {
@@ -20,5 +20,5 @@ export default defineConfig({
             changeOrigin: true
         }
     }
-  }
+  }*/
 })
