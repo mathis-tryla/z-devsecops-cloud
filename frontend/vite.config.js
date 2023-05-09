7259197eec73e198c5b8d8467536c1dca57d4a7c
Mathis_Tryla <mathistryla@gmail.com>
2023-05-09 16:21:25 +0200

    fix(bad-gateway-vuejs-accept-header): change from 80 to expose 3000 vuejs server object

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000/*,
+    port: 80/*,
     proxy: {
         '/api': {
             target: process.env.BASE_URL || 'http://localhost:8080',
