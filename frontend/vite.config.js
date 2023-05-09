8c937be3676a59864a281d411636232c77fa77b3
Mathis_Tryla <mathistryla@gmail.com>
2023-05-09 16:45:54 +0200

    fix(bad-gateway-vuejs-accept-header): add host value server vuejs object

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000/*,
+    port: 3000,
+    host: process.env.BASE_URL || 'http://localhost:8080'/*,
     proxy: {
         '/api': {
             target: process.env.BASE_URL || 'http://localhost:8080',
