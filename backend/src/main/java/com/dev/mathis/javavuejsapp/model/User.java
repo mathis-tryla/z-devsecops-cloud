b8d4aaa3ead5604e7cf8c6b374568f4a0a723e5b
Mathis_Tryla <mathistryla@gmail.com>
2023-05-24 14:36:18 +0200

    fix(auth): get user by id

--- backend/src/main/java/com/dev/mathis/javavuejsapp/model/User.java
+++ backend/src/main/java/com/dev/mathis/javavuejsapp/model/User.java
@@ -3,6 +3,8 @@ package com.dev.mathis.javavuejsapp.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 import javax.persistence.*;
 import jakarta.persistence.Entity;
 
@@ -12,12 +14,16 @@ import jakarta.persistence.Entity;
 @Setter
 public class User {
 
+    @jakarta.persistence.Id
+    @GeneratedValue
+    private Long id;
+
     /**
      * Username field relates to the username form input
      * where users can type their username or their email.
      * It is what makes the user unique.
      */
-    @jakarta.persistence.Id
+    //@jakarta.persistence.Id
     @Column(name = "USERNAME")
     private String username;
 
@@ -29,5 +35,15 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public boolean equals(User user) {
+        if (this == user)
+            return true;
+        if (!(user instanceof User))
+            return false;
+        User u = (User) user;
+        return Objects.equals(this.id, u.id) && Objects.equals(this.username, u.username)
+            && Objects.equals(this.password, u.password);
     }
 }
--- backend/src/main/java/com/dev/mathis/javavuejsapp/service/UserService.java
+++ backend/src/main/java/com/dev/mathis/javavuejsapp/service/UserService.java
@@ -2,11 +2,14 @@ package com.dev.mathis.javavuejsapp.service;
 
 import com.dev.mathis.javavuejsapp.model.User;
 import com.dev.mathis.javavuejsapp.repository.UserRepository;
+
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,7 +26,9 @@ public class UserService {
     }
 
     @Transactional
-    public User findUser(@RequestBody User user) { return userRepository.findUser(user); }
+    public Optional<User> findUserById(@PathVariable Long id) { 
+        return userRepository.findById(id);
+    }
 
     @Transactional
     public User createUser(@RequestBody User newUser) {
