ce71504b3977daf2c1f539136f6b76afc5d69db8
Mathis_Tryla <mathistryla@gmail.com>
2023-08-02 16:12:07 +0200

    Split terraform actions into two workflows

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -26,19 +26,9 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-/*provider "kubernetes" {
   host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
   )
-}*/
+}
--- terraform/traefik.tf
+++ terraform/traefik.tf
@@ -3,4 +3,6 @@ resource "helm_release" "traefik" {
   namespace  = "traefik"
   repository = "https://helm.traefik.io/traefik"
   chart      = "traefik"
+
+  depends_on = [google_container_cluster.primary, google_container_node_pool.primary_preemptible_nodes, kubernetes_namespace.traefik]
 }
